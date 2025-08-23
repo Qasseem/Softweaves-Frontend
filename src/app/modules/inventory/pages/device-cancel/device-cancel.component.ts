a0ac0de6127e1486eed633d3a6c753b4080f1598
Qassem <qasem@afaqy.com>
2025-08-23 19:15:28 +0300

    fix ui issues

--- src/app/modules/inventory/pages/device-cancel/device-cancel.component.ts
+++ src/app/modules/inventory/pages/device-cancel/device-cancel.component.ts
@@ -119,6 +119,7 @@ export class DeviceCancelComponent implements OnInit {
       condition: this.service.getConditionDropDown(),
       deployReceipt: this.ticketService.GetCancellationReceiptModelTypes(),
       warehouse: this.warehouseService.getAgentWarehouseDropDown(),
+      users: this.userService.getAllUsers(),
       // add remaining lookups
     }).subscribe((results) => {
       this.currencies = results.currency.data;
@@ -127,23 +128,8 @@ export class DeviceCancelComponent implements OnInit {
       this.receiptModels = results.deployReceipt.data;
       this.conditions = results.condition.data;
       this.warehouses = results.warehouse.data;
+      this.users = results.users.data;
     });
-  }
-
-  getUserByType(event) {
-    if (event && event.value) {
-      var userType = event.value;
-      this.userService
-        .getUsersByUserType(userType)
-        .pipe(take(1))
-        .subscribe((resp) => {
-          if (resp.success) {
-            this.users = resp.data;
-          }
-        });
-    } else {
-      this.users = [];
-    }
   }
 
   get f() {
