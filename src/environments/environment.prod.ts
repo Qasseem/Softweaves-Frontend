3e8f55d521ed98092d8eb90b0cfcf711cb9b9e89
Qasseem <qahmed120@gmail.com>
2026-03-08 16:19:14 +0200

    update tenantId & clientId

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -15,7 +15,7 @@ export const environment: EnvironmentConfiguration = {
     userProfile: 'user-profiles',
   },
   adConfig: {
-    clientId: '564e7c7a-3ad6-49bf-b757-169a8a999c98',
+    clientId: 'eca20e31-3c33-4253-88f5-f49666b7956f',
     readScopeUrl: 'api://b4b8a688-da1b-442d-9930-1d2d8ac59ebb/Read',
     writeScopeUrl: 'api://b4b8a688-da1b-442d-9930-1d2d8ac59ebb/Write',
     scopeUrls: [
@@ -23,7 +23,7 @@ export const environment: EnvironmentConfiguration = {
       'api://b4b8a688-da1b-442d-9930-1d2d8ac59ebb/Write',
     ],
     apiEndpointUrl: 'https://uatbe.softwaves.co',
-    tenantId: '03933b9c-4747-4902-8d69-82ec17bfc393',
+    tenantId: '97c70d5a-2f83-4e2d-9db0-7fe7a46c2898',
   },
   cacheTimeInMinutes: 15,
 };
